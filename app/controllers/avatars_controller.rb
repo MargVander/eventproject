059c4912c3796c36ee108e88121e5caa0109fdc9
MargVander <margauxvanderbeken@gmail.com>
2019-02-15 10:17:53 +0100

    aaaaaaaah

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -1,6 +1,6 @@
 class AvatarsController < ApplicationController
   def create
-    @user = User.find(params[:user_id])
+    @user = User.find(params["user_id"])
     @user.avatar.attach(params[:avatar])
     redirect_to user_path(@user.id)
   end
--- app/controllers/pictures_controller.rb
+++
@@ -1,7 +0,0 @@
-class AvatarsController < ApplicationController
-  def create
-    @event = Event.find(params[:event_id])
-    @event.picture.attach(params[:picture])
-    redirect_to(event_path(@event))
-  end
-end
