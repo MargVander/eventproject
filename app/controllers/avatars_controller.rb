ca280a52b9072a0b8312309bd8702f301a46f102
MargVander <margauxvanderbeken@gmail.com>
2019-02-15 09:38:31 +0100

    azertyu

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -2,6 +2,6 @@ class AvatarsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     @user.avatar.attach(params[:avatar])
-    redirect_to(user_path("user_id"))
+    redirect_to user_path(@user.id)
   end
 end
